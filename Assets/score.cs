5369e7fd422b80bcfe83d7f8e36ddd3be784d49c
bolu-oladele <boluoladele@aol.com>
2022-12-20 17:18:27 +0000

    Start Screen working, splat sound added

---
+++ Assets/score.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+public class ScoreCounter : MonoBehaviour {
+    
+    Ninja_Player player;
+    public Text scoreText = player.score;  
+ 
+    void Start () {
+        scoreText = GetComponent<Text>();  
+    }
+ 
+    void Update () {
+        scoreText.text = scoreText.text;
+    }
+}
