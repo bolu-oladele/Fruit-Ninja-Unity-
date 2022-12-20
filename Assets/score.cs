56d06fe55cd256c45d83ce2b791c656cb420f0de
bolu-oladele <boluoladele@aol.com>
2022-12-20 17:23:33 +0000

    Commented out score code

--- Assets/score.cs
+++ Assets/score.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using UnityEngine.UI; 
 public class ScoreCounter : MonoBehaviour {
     
-    Ninja_Player player;
-    public Text scoreText = player.score;  
+    // Ninja_Player player;
+    // public Text scoreText = player.score;  
  
-    void Start () {
-        scoreText = GetComponent<Text>();  
-    }
+    // void Start () {
+    //     scoreText = GetComponent<Text>();  
+    // }
  
-    void Update () {
-        scoreText.text = scoreText.text;
-    }
+    // void Update () {
+    //     scoreText.text = scoreText.text;
+    // }
 }
